940177ae22080d9aca3699eb41daf36268e3daec
MarioR516 <56093677+MarioR516@users.noreply.github.com>
2019-10-17 20:28:26 -0700

    Player Class

--- src/src/Player.java
+++ src/src/Player.java
@@ -1,5 +1,23 @@
 package src;
 
-public class Player {
 
+import java.util.ArrayList;
+
+public class Player {
+	String name;
+	ArrayList<Card> GainFoodRow;
+	ArrayList<FoodTokens> FoodTokens;
+	int eggs;
+	int ActionTokens;
+	int playerNum;
+	public Player(String _name, int _playerNum)
+	public void PlaceBirdCard(ArrayList<Card>) {
+		
+	}
+	public FoodTokens()
+	{
+		
+	}
+	
+	
 }
