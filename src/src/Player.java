e68344c5e140a7e790a883dd77ab82d6e3f1d47b
MarioR516 <56093677+MarioR516@users.noreply.github.com>
2019-11-08 11:15:08 -0800

    Player Handler

--- src/src/Player.java
+++ src/src/Player.java
@@ -1,7 +1,10 @@
 package src;
 //
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Player {
 	
@@ -10,24 +13,43 @@ public class Player {
 	int eggs;
 	public int ActionTokens = 8;
 	public List<Cards> GainFoodRow;
+	public List<Cards> GainEggsRow;
+	public List<Cards> GainCardsRow;
 	public List<Cards> Hand;
 	int playerNum;//// For putting a number on a player
-	
+	int eggCap;
+	int Invertabrate;
+	int Fruit;
+	int Fish;
+	int Rodent;
+	int Seed;
+	Map<String, Integer> Food;
 	public Player(String _name, int _playerNum)
 	{
 		Hand = new ArrayList<Cards>();
 		Cards fiveHand = new Cards();
 		//Dice foodTokensStart = new Dice();
 		GainFoodRow = new ArrayList<Cards>();
+		GainEggsRow = new ArrayList<Cards>();
+		GainCardsRow = new ArrayList<Cards>();
 		Hand = fiveHand.makeCard();
 		FoodTokens = new ArrayList<>();
-		FoodTokens.add("Invertabrate");
-		FoodTokens.add("Fish");
-		FoodTokens.add("Rodent");
-		FoodTokens.add("Seed");
-		FoodTokens.add("Fruit");
-		FoodTokens.add("Invertebrate_Seed");
 		name = _name;
+		eggCap = 0;
+		eggs = 0;
+		Invertabrate = 0;
+		Fruit = 0;
+		Fish = 0;
+		Rodent = 0;
+		Seed = 0;
+		Food = new HashMap<String, Integer>();
+		Food.put("Invertabrate", 0);
+		Food.put("Fruit",0);
+		Food.put("Seed",0);
+		Food.put("Rodent",0);
+		Food.put("Fish",0);
+		
+		
 		
 		
 		
@@ -47,10 +69,10 @@ public class Player {
 			amount = 2;
 			break;
 		case 3:
-			amount = 3;
+			amount = 2;
 			break;
 		case 4:
-			amount = 4;
+			amount = 3;
 			break;
 		default:
 			amount  = 0;
@@ -58,25 +80,340 @@ public class Player {
 		}
 		return amount;
 	}
-	public void PlaceBirdCard(int choice) //// Takes the GainFoodRow and places the bird in the next available slot
+	public int AmountofEggs()
 	{
-		GainFoodRow.add(Hand.get(choice));
-		Hand.remove(choice);
-		ActionTokens--;
-		System.out.println(name + "'s action tokens left : " +  ActionTokens);
+		int amount;
+		switch(GainEggsRow.size())
+		{
+		case 0:
+			amount = 1;
+			break;
+		case 1:
+			amount = 1;
+			break;
+		case 2:
+			amount = 2;
+			break;
+		case 3:
+			amount = 2;
+			break;
+		case 4:
+			amount = 3;
+			break;
+		default:
+			amount  = 0;
+			break;
+		}
+		return amount;
+	}
+	public int AmountofCards()
+	{
+		int amount;
+		switch(GainCardsRow.size())
+		{
+		case 0:
+			amount = 1;
+			break;
+		case 1:
+			amount = 1;
+			break;
+		case 2:
+			amount = 2;
+			break;
+		case 3:
+			amount = 2;
+			break;
+		case 4:
+			amount = 3;
+			break;
+		default:
+			amount  = 0;
+			break;
+		}
+		return amount;
+	}
+	public Boolean PutinForest(int choice) 
+	{
+		boolean check;
+		check = true;
+		int RowPos = GainFoodRow.size();
+		switch(RowPos)
+		{
+			case 0:
+				GainFoodRow.add(Hand.get(choice));
+				FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+				Hand.remove(choice);
+				check = true;
+				break;
+			case 1:
+				if(eggs >= 1)
+				{
+					GainFoodRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 1 eggs");
+					check = false;
+				}
+				break;
+			case 2:
+				if(eggs >= 1)
+				{
+					GainFoodRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 1 eggs");
+					check = false;
+				}
+				 break;
+			case 3:
+				if(eggs >= 1)
+				{
+					GainFoodRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 2 eggs");
+					check = false;
+				}
+				break;
+			case 4:
+				if(eggs >= 2)
+				{
+					GainFoodRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 2 eggs");
+					check = false;
+				}
+				break;
+			case 5:
+				System.out.println("Can't fit anymore birds into row");
+				break;
+				
+		}
+		return check;
+		
+	}
+	public Boolean PutinGrasslands(int choice) 
+	{
+		boolean check;
+		check = true;
+		int RowPos = GainEggsRow.size();
+		switch(RowPos)
+		{
+			case 0:
+				GainEggsRow.add(Hand.get(choice));
+				FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+				Hand.remove(choice);
+				check = true;
+				break;
+			case 1:
+				if(eggs >= 1)
+				{
+					GainEggsRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 1 eggs");
+					check = false;
+				}
+				break;
+			case 2:
+				if(eggs >= 1)
+				{
+					GainEggsRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 1 eggs");
+					check = false;
+				}
+				break;
+			case 3:
+				if(eggs >= 1)
+				{
+					GainEggsRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 2 eggs");
+					check = false;
+				}
+				break;
+			case 4:
+				if(eggs >= 2)
+				{
+					GainEggsRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 2 eggs");
+					check = false;
+				}
+				break;
+			case 5:
+				System.out.println("Can't fit anymore birds into row");
+				break;
+				
+				
+		}
+		return check;
+		
+	}
+	public Boolean PutinWetlands(int choice) 
+	{
+		boolean check;
+		check = true;
+		int RowPos = GainCardsRow.size();
+		switch(RowPos)
+		{
+			case 0:
+				GainCardsRow.add(Hand.get(choice));
+				FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+				Hand.remove(choice);
+				check = true;
+				break;
+			case 1:
+				if(eggs >= 1)
+				{
+					GainCardsRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 1 eggs");
+					check = false;
+				}
+				break;
+			case 2:
+				if(eggs >= 1)
+				{
+					GainCardsRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 1 eggs");
+					check = false;
+				}
+				break;
+			case 3:
+				if(eggs >= 1)
+				{
+					GainCardsRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 2 eggs");
+					check = false;
+				}
+				break;
+			case 4:
+				if(eggs >= 2)
+				{
+					GainCardsRow.add(Hand.get(choice));
+					FoodTokens.remove(FoodTokens.indexOf(Hand.get(choice).TypeOfFood));
+					Hand.remove(choice);
+					eggs--;
+					check = true;
+				}
+				else
+				{
+					System.out.println("Need 2 eggs");
+					check = false;
+				}
+				break;
+			case 5:
+				System.out.println("Can't fit anymore birds into row");
+				break;
+				
+		}
+		return check;
+		
+	}
+	public void checkAmountofFood()
+	{
+		if(!FoodTokens.isEmpty())
+		{
+			for(int i = 0; i < FoodTokens.size();i++)
+			{
+				Food.put(FoodTokens.get(i), +1);
+			}
+		}
+		else
+		{
+			
+		}
+		for (String keys : Food.keySet())  
+		{
+		   System.out.println(keys + ":"+ Food.get(keys));
+		}
 		
 	}
 	public void DisplayHand()
 	{
 		for(int i = 0; i < Hand.size(); i++)
 		{
-			System.out.println(i+" : " + Hand.get(i).getName() + "  " + " FoodType: " + Hand.get(i).TypeOfFood);
+			System.out.println(i+" : " + Hand.get(i).name + "  " + " FoodType: " + Hand.get(i).TypeOfFood);
 		}
 		System.out.println();
-		System.out.println("Available food: ");
-		for(int i = 0; i < FoodTokens.size();i++)
+		checkAmountofFood();
+		
+	}
+	public void DisplayHandAvail(String hab)
+	{
+		System.out.println("Cards available for " + hab);
+		for(int i = 0; i < Hand.size();i++)
 		{
-			System.out.println(FoodTokens.get(i));
+			if(Hand.get(i).Habitat ==  hab)
+			{
+				System.out.println(i + ") " + Hand.get(i).name);
+			}
 		}
 	}
 	
@@ -86,12 +423,34 @@ public class Player {
 	
 	public void ViewBoard()
 	{
-		System.out.print("Gain Food Row: ");
-		for(int i =0; i < GainFoodRow.size();i++)
+		System.out.println();
+		System.out.print("Forest: ");
+		if(GainFoodRow.size() > 0)
 		{
-			System.out.print( "["+ GainFoodRow.get(i).getName()+"]"+ "   ");
+			for(int i =0; i < GainFoodRow.size();i++)
+			{
+				System.out.print( "["+ GainFoodRow.get(i).name +"]"+ "   ");
+			}
 		}
 		System.out.println();
+		System.out.print("Grasslands: ");
+		if(GainEggsRow.size() > 0)
+		{
+			for(int i =0; i < GainEggsRow.size();i++)
+			{
+				System.out.print( "["+ GainEggsRow.get(i).name +"]"+ "   ");
+			}
+		}
+		System.out.println();
+		System.out.print("Wetlands: ");
+		if(GainCardsRow.size() > 0)
+		{
+			for(int i =0; i < GainCardsRow.size();i++) {
+				System.out.print("["+ GainCardsRow.get(i).name +"]"+ "   ");
+			}
+		}
+		System.out.println();
+
 	}
 	public int score()///Evaluates the score for the player.
 	{
