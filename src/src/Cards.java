2c73c615176738f949e0bc2f751080b4a1dd822c
MarioR516 <56093677+MarioR516@users.noreply.github.com>
2019-11-08 11:15:32 -0800

    Card Handler

--- src/src/Cards.java
+++ src/src/Cards.java
@@ -1,78 +1,42 @@
 package src;
-import java.io.*;
 import java.util.*;
 
 public class Cards {
 		
 		//Attributes of a card
-		String name;
+		public String name;
 		public int egg;
 		public int cost;
-		public char habitat;
+		public String Habitat;
 		public String power;
 		public String TypeOfFood;
 		
 		//Default constructor
-		Cards(){
-		this.name = "Birds";
-		this.egg = 2;
-		this.cost = 3;
-		this.habitat = 'w';
-		this.power = "Activate";
-		}
+
 		//
 		//Constructor that initializes class variables
-		Cards(int e,String n,int c,char h,String p , String Food){
+		Cards()
+		{
+			
+		}
+		Cards(int e,String n,int c,String h,String p , String Food){
 			this.egg = e;
 			this.name = n;
 			this.cost = c;
-			this.habitat = h;
+			this.Habitat = h;
 			this.power = p;
 			TypeOfFood = Food;
 		}
 		
-		 //Setters
-		 public void setEgg(int e) {
-			    this.egg = e;
-			  }
-		 public void setName(String n) {
-			    this.name = n;
-			  }
-		 public void setCost(int c) {
-			    this.cost = c;
-			  }
-		 public void setHabitat(char h) {
-			    this.habitat = h;
-			  }
-		 public void setPower(String p) {
-			    this.power = p;
-			  }
-		 
-		 //Getters
-		 public int getEgg() {
-			    return egg;
-			  }
-		 public String getName() {
-			    return name;
-			  }
-		 public int getCost() {
-			    return cost;
-			  }
-		 public char getHabitat() {
-			    return habitat;
-			  }
-		 public String getPower() {
-			    return power;
-			  }
-		 
+
 		 public List<Cards> makeCard(){
 			 //int e,String n,int c,char h,String p
 			 List<Cards> FiveCards = new ArrayList<Cards>();
-			 Cards first = new Cards(1,"Steller's Jay",3,'w',"Activate","Invertebrate_Seed");
-			 Cards second = new Cards(2,"American White Pelican",4,'d',"Extra egg","Fish");
-			 Cards third = new Cards(3,"Eastern KingBird",6,'w',"Once between turns","Seed");
-			 Cards fourth = new Cards(4,"Mountain BlueBird",7,'d',"Play a second bird","Rodent");
-			 Cards fifth = new Cards(5,"American Kestrel",8,'w',"Roll a dice","Fruit");
+			 Cards first = new Cards(1,"Steller's Jay",3,"Wetland","Activate","Invertebrate_Seed");
+			 Cards second = new Cards(2,"American White Pelican",4,"Grassland","Extra egg","Fish");
+			 Cards third = new Cards(3,"Eastern KingBird",6,"Forest","Once between turns","Seed");
+			 Cards fourth = new Cards(4,"Mountain BlueBird",7,"Wetland","Play a second bird","Rodent");
+			 Cards fifth = new Cards(5,"American Kestrel",8,"Forest","Roll a dice","Fruit");
 			 FiveCards.add(first);
 			 FiveCards.add(second);
 			 FiveCards.add(third);
@@ -80,15 +44,6 @@ public class Cards {
 			 FiveCards.add(fifth);
 			 return FiveCards;
 		    }
-		
-		//To string method that displays a card
-		public String toString(){
-		      return "Name: "+ getName() +
-		    		 "\nEgg: "+ getEgg() + 
-		    		 "\nCost: "+ getCost() +
-		    		 "\nHabitat: "+ getHabitat() +
-		    		 "\nName: "+ getPower() +"\n\n"
-		    		 ;
-		    }
+
 	}
 
