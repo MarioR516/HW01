a2d4e9478003a4f5b64906a53451b310d1d9f5b8
F471m4 <40557297+fatima-mo@users.noreply.github.com>
2019-11-14 16:03:56 -0800

    Update testingtool.py

--- UnitTest/testingtool.py
+++ UnitTest/testingtool.py
@@ -2,9 +2,9 @@ import json
 import subprocess
 with open("Test.json") as json_file:
     data = json.load(json_file)
-    for p in data["Classes"]:
-        print("Name: " + p["Name"] + "compiling.")
+    for p in data["cases"]:
+        print("name: " + p["name"] + "compiling.")
         subprocess.call(['javac -cp junit-4.13-rc-1.jar'], shell=True)
-        for p in data["Classes"]:
-            print("Name: " + p["Name"] + "compiled.")
+        for p in data["cases"]:
+            print("name: " + p["name"] + "compiled.")
             sub.call('java -cp junit-4.13-rc-1.jar; hamcrest.core_1.3.0.v201303031735.jar'], shell=True)
